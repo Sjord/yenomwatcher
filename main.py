bd7dfdb81ee672c1122f714bad88d6495f1f1824
Sjoerd Langkemper <sjoerd@byte.nl>
2015-12-18 12:23:30 +0100

    Output something when there is a discount on Absolut

---
+++ main.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = 'http://www.yenom.nl/slijterijaanbiedingen/search/111/absolut'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+prices = soup.select('.hikashop_product_price_with_discount')
+for elem in prices:
+    m = re.match(u'€ (\d+),\d+', elem.string)
+    euro_price = int(m.group(1))
+    if euro_price < 16:
+        print elem.string, url
