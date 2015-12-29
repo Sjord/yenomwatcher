d5c109069559345af5e6ceb5e0bd3d3e8af02705
Sjoerd <sjoerd-github@linuxonly.nl>
2015-12-29 20:38:27 +0100

    Get better price, print store and action

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 url = 'http://www.yenom.nl/slijterijaanbiedingen/search/111/absolut'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
-prices = soup.select('.hikashop_product_price_with_discount')
+prices = soup.select('.hikashop_product_price_0')
 for elem in prices:
     m = re.match(u'€ (\d+),\d+', elem.string)
     euro_price = int(m.group(1))
     if euro_price < 16:
-        print elem.string, url
+        text = elem.find_parent('tr').select_one('.hikashop_product_name_row em').string
+        print elem.string, text
